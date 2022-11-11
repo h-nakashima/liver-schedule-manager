5a1dd52a9c8ea190fcbe99ad690a2e7b1da3e454
h-nakashima <h-nakashima@users.noreply.github.com>
2022-11-11 15:25:49 +0900

    レスポンスの中身を見るテストケース追加

--- test/examples.controller.ts
+++ test/examples.controller.ts
@@ -8,8 +8,13 @@ describe("Events", () => {
     request(Server)
       .get("/api/v1/events")
       .expect("Content-Type", /json/)
-      .then((r) => {
-        expect(r.body).to.be.an("array").of.length(2);
+      .then((response) => {
+        const expected = [
+          { id: 1, name: "event 0" },
+          { id: 2, name: "event 1" },
+        ];
+        expect(response.body).to.be.an("array").of.length(2);
+        expect(response.body).to.be.deep.equal(expected);
       }));
 
   it("should add a new event", () =>

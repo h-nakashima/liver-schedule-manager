5ac6386f354f1f4de21076b533c9f60e0700d8ba
h-nakashima <h-nakashima@users.noreply.github.com>
2022-11-11 15:00:11 +0900

    refactor: ファイル名、変数名を変更

--- server/api/controllers/examples/controller.ts
+++ server/api/controllers/examples/controller.ts
@@ -1,6 +1,6 @@
 /* eslint-disable class-methods-use-this */
 import { Request, Response } from "express";
-import EventsService from "../../services/examples.service";
+import EventsService from "../../services/events.service";
 
 export class Controller {
   all(_: Request, res: Response): void {
--- test/examples.controller.ts
+++ test/examples.controller.ts
@@ -12,7 +12,7 @@ describe("Events", () => {
         expect(r.body).to.be.an("array").of.length(2);
       }));
 
-  it("should add a new example", () =>
+  it("should add a new event", () =>
     request(Server)
       .post("/api/v1/events")
       .send({ name: "test" })
@@ -24,7 +24,7 @@ describe("Events", () => {
           .equal("test");
       }));
 
-  it("should get an example by id", () =>
+  it("should get an event by id", () =>
     request(Server)
       .get("/api/v1/events/2")
       .expect("Content-Type", /json/)

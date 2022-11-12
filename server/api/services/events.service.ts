a68aea60e88c0751c68fd684bdc81c7fec20a8d8
h-nakashima <h-nakashima@users.noreply.github.com>
2022-11-12 14:15:05 +0900

    fix: rename

--- server/api/services/events.service.ts
+++ server/api/services/events.service.ts
@@ -14,17 +14,17 @@ const events: Event[] = [
 
 export class EventsService {
   all(): Promise<Event[]> {
-    L.info(events, "fetch all examples");
+    L.info(events, "fetch all events");
     return Promise.resolve(events);
   }
 
   byId(id: number): Promise<Event> {
-    L.info(`fetch example with id ${id}`);
+    L.info(`fetch event with id ${id}`);
     return this.all().then((r) => r[id]);
   }
 
   create(name: string): Promise<Event> {
-    L.info(`create example with name ${name}`);
+    L.info(`create event with name ${name}`);
     const event: Event = {
       id: currentId + 1,
       name,
